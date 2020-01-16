2c89074f58e50bca7d1da950acd487b6f0d93a71
kharya1996 <kharyashashank02@gmail.com>
2020-01-16 15:56:16 +0530

    correction Palindrome

--- Palindrome_String.java
+++ Palindrome_String.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class Palindrome_String {
 
 	public static void main(String[] args) {
-		String str, reverse = "";
+		String user_input, reverse = "";
 		Scanner input =new Scanner(System.in);
 		System.out.println("Enter any String to it's Palindrome");
-		str = input.nextLine();
+		user_input = input.nextLine();
 		
-		for(int i = str.length()-1; i>=0; i--) {
+		for(int i = user_input.length()-1; i>=0; i--) {
 		
-			reverse = reverse + str.charAt(i);	
+			reverse = reverse + user_input.charAt(i);	
 			
 		}
 		
-		if(reverse.equals(str)) {
+		if(reverse.equals(user_input)) {
 			
 			System.out.println("This string is Palindrome");
 		}
