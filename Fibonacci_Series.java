e98fdaeb936eb4a34b898799875a759bbb551d7d
kharya1996 <kharyashashank02@gmail.com>
2020-01-16 16:16:49 +0530

    correction

--- Fibonacci_Series.java
+++ Fibonacci_Series.java
@@ -3,16 +3,16 @@ public class Fibonacci_Series {
 
 	public static void main(String[] args) {
 		
-		int a =0, b =1 ,c;
+		int num1 =0, num2 =1 ,Result;
 		
-		System.out.print(a+" "+b);
+		System.out.print(num1+" "+num2);
 		for(int i = 0; i<=20; i++) {
 			
-			c = a + b;
+			Result = num1 + num2;
 			
-			System.out.print(" "+c);
-			a = b; 
-			b = c;
+			System.out.print(" "+Result);
+			num1 = num2; 
+			num2 = Result;
 			
 		} 
 		
