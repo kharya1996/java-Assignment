e08bf52b7a3bd35f64f3361612312af2b8fb7f7a
shashank kharya <52448463+kharya1996@users.noreply.github.com>
2020-01-19 12:44:43 +0530

    Update Calculator.java
    Changes in operation input

--- Calculator.java
+++ Calculator.java
@@ -3,60 +3,64 @@ import java.util.Scanner;
 public class Calculator {
 
 	public static void main(String[] args) {
-	
-		int Num_1 ,Num_2, Result;
+
+		int Num_1, Num_2, Result;
 		String symbol, toContinue;
-		
-			
+
 		do {
 			Scanner input = new Scanner(System.in);
 			System.out.println("CALCULATOR:-");
 			System.out.println("Enter the first No:");
 			Num_1 = input.nextInt();
-			
+
 			System.out.println("Enter the Second No:");
 			Num_2 = input.nextInt();
-			
+
 			System.out.println("Enter the operation ( + , - , * , / ) ");
 			symbol = input.next();
+
+			if (symbol ==("+") || symbol ==("-") || symbol ==("*") || symbol ==("/")); {
+
+				switch (symbol) {
+
+				case "+":
+					Result = Num_1 + Num_2;
+					System.out.println("Result Addition is " + Result);
+					break;
+
+				case "-":
+					Result = Num_1 - Num_2;
+					System.out.println("Result Subtraction is " + Result);
+					break;
+
+				case "/":
+					if (Num_2 != 0) {
+						Result = Num_1 / Num_2;
+						System.out.println("Result Divison is " + Result);
+					} else {
+
+						System.out.println("Number Can't Divide by 0");
+					}
+					break;
+				case "*":
+					Result = Num_1 * Num_2;
+					System.out.println("Result Multiplication is " + Result);
+					break;
+				default:
+					System.out.println("Input is invalid");
+					break;
+
+				}
+			} 
 			
-			switch(symbol) {
-			
-			case"+":Result = Num_1 + Num_2;
-				System.out.println("Result Addition is " +Result);
-				break;
-			
-			case"-": Result = Num_1 - Num_2;
-				System.out.println("Result Subtraction is " +Result);
-				break;
-			
-			case"/": if(Num_2!=0) {
-				Result = Num_1 / Num_2;
-				System.out.println("Result Divison is " +Result);
-			}else {
-				
-				System.out.println("Number Can't Divide by 0");
-			}
-				break;
-			case"*": Result = Num_1 * Num_2;
-				System.out.println("Result Multiplication is " +Result);
-				break;
-			default:System.out.println("Input is invalid");
-			break;
-			
-			}
+
 			System.out.println("Do you want do more operation Press Y for YES  or N for No ");
 			toContinue = input.next();
-			
-		}
-			while(toContinue.equals("y") || toContinue.equals("Y") );
-		
-		
-				
-		
-		}	
-		
-		
+
+		} while (toContinue.equals("y") || toContinue.equals("Y"));
+
 	}
+
+}
 
 
